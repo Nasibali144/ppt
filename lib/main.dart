0ec472742c9283c093a18ff5e9ff6dced1f797f2
Nasibali144 <abdiyevnasibali@gmail.com>
2023-07-19 11:54:45 +0500

    Five Commit: custom_provider.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,192 @@
-/*
 import 'package:flutter/material.dart';
-import 'package:ppt/app.dart';
-
-import 'core/setup_locator.dart';
+import 'package:ppt/provider/custom_provider.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  serviceLocator();
-  runApp(const PostApp());
-}*/
-
-import 'package:flutter/material.dart';
+  runApp(const MyApp());
+}
 
 
+class User {
+  String name;
+  String email;
 
-void main() => runApp(const MyApp());
+  User({required this.name, required this.email});
+}
+
+class UserController extends ChangeNotifier {
+  User? user;
+
+  void signIn(String email, String name) {
+    user = User(name: name, email: email);
+    notifyListeners();
+  }
+
+  void signUp(String email, String name) {
+    user = User(name: name, email: email);
+    notifyListeners();
+  }
+
+  void signOut() {
+    user = null;
+    notifyListeners();
+  }
+
+  bool get isAuth => user != null;
+}
+
+class ThemeController extends ChangeNotifier {
+  bool isLight = true;
+
+  void changeTheme() {
+    isLight = !isLight;
+    notifyListeners();
+  }
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(useMaterial3: true),
-      home: const HomePage(),
+    return NotifierProvider<UserController>(
+      value: UserController(),
+      child: NotifierProvider<ThemeController>(
+        value: ThemeController(),
+        child: Builder(builder: (context) {
+          return MaterialApp(
+            theme: ThemeData.light(useMaterial3: true),
+            darkTheme: ThemeData.dark(useMaterial3: true),
+            themeMode: context.watch<ThemeController>().isLight ? ThemeMode.light : ThemeMode.dark,
+            home: const SplashPage(),
+          );
+        }),
+      ),
     );
   }
 }
 
-
-class Provider extends InheritedWidget {
-  final int value;
-
-  const Provider({
-    Key? key,
-    required this.value,
-    required Widget child,
-  }) : super(key: key, child: child);
-
-  static Provider? of(BuildContext context, {bool listen = false}) => listen
-      ? context.dependOnInheritedWidgetOfExactType<Provider>()
-      : context.getElementForInheritedWidgetOfExactType<Provider>()?.widget as Provider?;
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
 
   @override
-  bool updateShouldNotify(Provider oldWidget) => value != oldWidget.value;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => SignInPage()));
+          },
+          style: Theme.of(context).elevatedButtonTheme.style,
+          child: const Text("Get Stated"),
+        ),
+      ),
+    );
+  }
+}
+
+class SignInPage extends StatelessWidget {
+  SignInPage({Key? key}) : super(key: key);
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SignIn"),
+      ),
+      body: SafeArea(
+        minimum: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(hintText: "Name"),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(hintText: "Email"),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    ),
+                  );
+                  context.read<UserController>().signIn(emailController.text, nameController.text);
+                },
+                style: Theme.of(context).elevatedButtonTheme.style,
+                child: const Text("SignIn"),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => SignUpPage()));
+                },
+                style: Theme.of(context).elevatedButtonTheme.style,
+                child: const Text("Go SingUp"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpPage extends StatelessWidget {
+  SignUpPage({Key? key}) : super(key: key);
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sign Up"),
+      ),
+      body: SafeArea(
+        minimum: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(hintText: "Name"),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(hintText: "Email"),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  NotifierProvider.of<UserController>(context)
+                      .signUp(emailController.text, nameController.text);
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => SignInPage()));
+                },
+                style: Theme.of(context).elevatedButtonTheme.style,
+                child: const Text("SignUp"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class HomePage extends StatelessWidget {
@@ -53,87 +194,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: DataOwnerStateFull(),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          Consumer<ThemeController>(builder: (context, provider, _) {
+            return IconButton(
+              onPressed: () {
+                provider.changeTheme();
+              },
+              icon: Icon(provider.isLight ? Icons.sunny : Icons.dark_mode),
+            );
+          })
+        ],
       ),
-    );
-  }
-}
-
-class DataOwnerStateFull extends StatefulWidget {
-  const DataOwnerStateFull({Key? key}) : super(key: key);
-
-  @override
-  State<DataOwnerStateFull> createState() => _DataOwnerStateFullState();
-}
-
-class _DataOwnerStateFullState extends State<DataOwnerStateFull> {
-  var _value = 0;
-
-  void _increment() {
-    _value += 1;
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-          onPressed: _increment,
-          child: const Text("Tap"),
+      body: Center(
+        child: Consumer<UserController>(
+          builder: (context, controller, _) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(controller.user?.name ?? "",
+                    style: Theme.of(context).textTheme.titleMedium),
+                const SizedBox(height: 100),
+                Text(controller.user?.email ?? "",
+                    style: Theme.of(context).textTheme.titleMedium),
+                const SizedBox(height: 100),
+                ElevatedButton(
+                  onPressed: () {
+                    controller.signOut();
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => SignInPage()));
+                  },
+                  style: Theme.of(context).elevatedButtonTheme.style,
+                  child: const Text("SingOut"),
+                ),
+              ],
+            );
+          }
         ),
-        Provider(
-          value: _value,
-          child: const DataConsumerStateless(),
-        ),
-      ],
-    );
-  }
-}
-
-class DataConsumerStateless extends StatelessWidget {
-  const DataConsumerStateless({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    //! first method
-    final value = Provider.of(context, listen: false)?.value ?? 0;
-    return Column(
-      children: [
-        Text(
-          "$value",
-          style: const TextStyle(
-            fontSize: 30,
-          ),
-        ),
-        const DataConsumerStateFull(),
-      ],
-    );
-  }
-}
-
-class DataConsumerStateFull extends StatefulWidget {
-  const DataConsumerStateFull({Key? key}) : super(key: key);
-
-  @override
-  State<DataConsumerStateFull> createState() => _DataConsumerStateFullState();
-}
-
-class _DataConsumerStateFullState extends State<DataConsumerStateFull> {
-  @override
-  Widget build(BuildContext context) {
-    //! second method
-    final value = Provider.of(context, listen: true)?.value ?? 0;
-
-    return Text(
-      "$value",
-      style: const TextStyle(
-        fontSize: 30,
       ),
     );
   }
