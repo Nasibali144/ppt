ed7f09e4862e2c1a1d8f2febd5ec373f4e672c65
Nasibali144 <abdiyevnasibali@gmail.com>
2023-07-20 11:53:41 +0500

    Todo Project with Provider: Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ppt/controllers/home_controller.dart';
 import 'package:ppt/core/service_locator.dart';
+import 'package:ppt/models/todo/todo.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -17,22 +18,21 @@ class HomeScreen extends StatelessWidget {
           ),
           body: Consumer<HomeController>(
             builder: (context, controller, child) {
-              return ListView.builder(
-                padding: const EdgeInsets.all(20),
-                itemCount: controller.items.length,
-                itemBuilder: (context, index) {
-                  final todo = controller.items[index];
-                  return Card(
-                    child: ListTile(
-                      title: Text(todo.title),
-                      subtitle: Text(todo.description),
-                      trailing: Checkbox(
-                        value: todo.isCompleted,
-                        onChanged: (value) {},
-                      ),
-                    ),
-                  );
-                },
+              return Stack(
+                children: [
+                  ListView.builder(
+                    padding: const EdgeInsets.all(20),
+                    itemCount: controller.items.length,
+                    itemBuilder: (context, index) {
+                      final todo = controller.items[index];
+                      return ItemCard(todo: todo);
+                    },
+                  ),
+
+                  if(controller.isLoading) const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ],
               );
             },
           ),
@@ -41,3 +41,23 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+class ItemCard extends StatelessWidget {
+  final Todo todo;
+  const ItemCard({Key? key, required this.todo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(todo.title),
+        subtitle: Text(todo.description),
+        trailing: Checkbox(
+          value: todo.isCompleted,
+          onChanged: (value) {},
+        ),
+      ),
+    );
+  }
+}
+
